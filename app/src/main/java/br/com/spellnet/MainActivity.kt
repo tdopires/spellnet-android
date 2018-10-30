1ab127c3e970179a598631559a4e2b51afad9956
tdopires <tdopires@gmail.com>
2018-10-29 22:11:40 -0300

    restarting from scratch

---
+++ app/src/main/java/br/com/spellnet/MainActivity.kt
@@ -0,0 +1,19 @@
+package br.com.spellnet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.spellnet.ui.decklist.DeckListFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, DeckListFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/br/com/spellnet/ui/decklist/DeckListViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.spellnet.ui.decklist
+
+import android.arch.lifecycle.ViewModel
+
+class DeckListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
